ba5d49b35e9f1a6c720e2e08d70acc0d523c3e2d
BdoRosales <166651046+BdoRosales@users.noreply.github.com>
2026-05-14 18:05:07 -0600

    Membresias v2

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import MembresiasView from '../views/MembresiasView.vue' 
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
